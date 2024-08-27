b956138c1de801f39c6f2771b4dae76f0cafe7cc
FidelCastro007 <118585134+FidelCastro007@users.noreply.github.com>
2024-08-27 23:23:08 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,8 +7,6 @@ const User = require('./model/User')
 require('dotenv').config()
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-console.log('Port:', process.env.PORT);
 
 //Middleware
 app.use(bodyParser.json());
